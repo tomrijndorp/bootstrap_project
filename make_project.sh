8cf0555e750e3f260bd69946ff83da13626056f9
Tom Rijndorp <tomrijndorp@gmail.com>
2020-11-21 15:10:56 -0800

    Enable working with absolute paths, include help, prevent overwrites

--- make_project.sh
+++ make_project.sh
@@ -2,23 +2,34 @@
 set -euxo pipefail
 
 say() {
-    echo -e "\033[1m$1\033[0m"
+    echo -e "\033[1m$@\033[0m"
 }
 
-say "Creating Bazel C++ project in $(realpath ./$project_name)..."
-# copy templates to partcular folder
-# replace instances of project name if necessary
+if [[ $# -lt 1 ]]; then
+    say "Usage: $0 <path>"
+    exit 1
+fi
 
-# the output directory will be $(pwd)/project_name.
+# the output directory will be $project_name (so either relative to
+# cwd or absolute path)
 project_name=$1
+
+if [[ $(stat "$project_name") ]]; then
+    say "$project_name" already exists
+    exit 1
+fi
+
+say "Creating Bazel C++ project in <$project_name>..."
+# copy templates to particular folder
+# replace instances of project name if necessary
 
 here=$(dirname "$0")
 templates="$here/templates"
 
-cp -r "$templates" "./$project_name"
+cp -r "$templates" "$project_name"
 
 # Copy into the project
-cd "./$project_name"
+cd "$project_name"
 
 say "Testing project..."
 # Test
